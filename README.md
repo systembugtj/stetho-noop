0c1ca4918bb975ef3c83b182de252b68beed2c92
systembugtj <albert_lee@hotmail.com>
2018-06-04 09:53:49 -0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# stetho-debug
+
+[![](https://jitpack.io/v/systembugtj/stetho-noop.svg)](https://jitpack.io/#systembugtj/stetho-noop)
+
+A simple wrapper to allow release usage of stetho.
---
+++ stetho/src/main/java/com/systembug/stetho/StethoSetup.java
@@ -0,0 +1,13 @@
+package com.systembug.stetho;
+
+import android.content.Context;
+
+import okhttp3.OkHttpClient;
+
+public class StethoSetup {
+    public static void setup(Context context) {
+    }
+
+    public static void setupOkhttp(OkHttpClient.Builder builder) {
+    }
+}
